0605b0f4481bd39dd59fc6c5f3237aff2081c353
Jelte Dirks <j.dirks@outlook.com>
2023-05-30 22:45:26 +0200

    add descriptions to harpoon keymaps
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,31 +1,31 @@
-local nmap = function(keys, func)
-  vim.keymap.set('n', keys, func)
+local nmap = function(keys, func, desc)
+  vim.keymap.set('n', keys, func, { desc = desc })
 end
 
 nmap('<leader>j', function()
   require('harpoon.ui').nav_file(1)
-end)
+end, "Harpoon go to file 1")
 
 nmap('<leader>k', function()
   require('harpoon.ui').nav_file(2)
-end)
+end, "Harpoon go to file 2")
 
 nmap('<leader>l', function()
   require('harpoon.ui').nav_file(3)
-end)
+end, "Haproon go to file 3")
 
 nmap('<leader>;', function()
   require('harpoon.ui').nav_file(4)
-end)
+end, "Harpoon go to file 4")
 
 nmap('<leader>mc', function()
   require('harpoon.mark').clear_all()
-end)
+end, "Harpoon clean all marks")
 
 nmap('<leader>mm', function()
   require('harpoon.mark').add_file()
-end)
+end, "Harpoon add file")
 
 nmap('<leader>mu', function()
   require("harpoon.ui").toggle_quick_menu()
-end)
+end, "Harpoon show menu")
