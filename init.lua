8261b20f8c167dc386b10ff95fab3f784a7916fe
Jelte Dirks <j.dirks@outlook.com>
2024-05-15 23:51:15 +0200

    disable inlay hints and set catpuccin again
    Also adds gruvbox as an option.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- init.lua
+++ init.lua
@@ -317,7 +317,7 @@ require("lazy").setup({
 					end
 
 					if client and client.server_capabilities.inlayHintProvider then
-						vim.lsp.inlay_hint.enable(event.buf, true)
+						vim.lsp.inlay_hint.enable(event.buf, false) -- disable for now
 					end
 				end,
 			})
@@ -552,13 +552,16 @@ require("lazy").setup({
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
 		opts = {
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
 			background = {
 				light = "latte",
 				dark = "mocha",
 			},
-			transparent_background = true,
+			transparent_background = false,
 			show_end_of_buffer = false, -- show the '~' characters after the end of buffers
 			no_italic = false, -- Force no italic
 			no_bold = false, -- Force no bold
@@ -595,10 +598,6 @@ require("lazy").setup({
 				},
 			},
 		},
-		lazy = true,
-		-- init = function()
-		-- 	vim.cmd.colorscheme("catppuccin")
-		-- end,
 	},
 
 	{ -- You can easily change to a different colorscheme.
@@ -607,15 +606,24 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
-		priority = 1000,
+		lazy = true,
+		config = true,
 		init = function()
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			--vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			--vim.cmd.hi("Comment gui=none")
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		config = true,
+		lazy = true,
+		init = function()
+			-- vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 
