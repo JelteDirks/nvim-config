2e510fd219d3fb96717bac7bdc04429f3f326bde
Jelte Dirks <j.dirks@outlook.com>
2024-05-16 00:20:11 +0200

    remove comments, transparant bg
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- init.lua
+++ init.lua
@@ -324,16 +324,6 @@ require("lazy").setup({
 
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				clangd = {},
 				pyright = {},
@@ -348,9 +338,6 @@ require("lazy").setup({
 				},
 				tsserver = {},
 				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
@@ -561,7 +548,7 @@ require("lazy").setup({
 				light = "latte",
 				dark = "mocha",
 			},
-			transparent_background = false,
+			transparent_background = true,
 			show_end_of_buffer = false, -- show the '~' characters after the end of buffers
 			no_italic = false, -- Force no italic
 			no_bold = false, -- Force no bold
@@ -609,12 +596,7 @@ require("lazy").setup({
 		lazy = true,
 		config = true,
 		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			--vim.cmd.colorscheme("tokyonight-night")
-
-			-- You can configure highlights by doing something like:
 			--vim.cmd.hi("Comment gui=none")
 		end,
 	},
