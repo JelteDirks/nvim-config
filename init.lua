a8021454d3fb84184edcae4c9a2a7f8e76c78943
Jelte Dirks <j.dirks@outlook.com>
2024-05-16 00:39:04 +0200

    remove mini nvim, dont seem to use it
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- init.lua
+++ init.lua
@@ -627,38 +627,6 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "Telescope the todo list" })
 		end,
 	},
-
-	{ -- Collection of various small independent plugins/modules
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require("mini.surround").setup({
-				mappings = {
-					add = "sa", -- Add surrounding in Normal and Visual modes
-					delete = "sd", -- Delete surrounding
-					find = "sf", -- Find surrounding (to the right)
-					find_left = "sF", -- Find surrounding (to the left)
-					highlight = "", -- Highlight surrounding
-					replace = "sr", -- Replace surrounding
-					update_n_lines = "sn", -- Update `n_lines`
-					suffix_last = "l", -- Suffix to search with "prev" method
-					suffix_next = "n", -- Suffix to search with "next" method
-				},
-			})
-		end,
-	},
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = {
