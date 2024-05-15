f1e0a8d04710373ea353bc1bb0f64c95733e0302
Jelte Dirks <j.dirks@outlook.com>
2024-05-16 00:53:06 +0200

    remove some ruby stuff
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- init.lua
+++ init.lua
@@ -688,12 +688,8 @@ require("lazy").setup({
 			auto_install = false,
 			highlight = {
 				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
 			},
-			indent = { enable = true, disable = { "ruby" } },
+			indent = { enable = true },
 		},
 		config = function(_, opts)
 			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
