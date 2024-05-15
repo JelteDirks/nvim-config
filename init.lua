3586be48930f9224e603d77a39007959ef3d8802
Jelte Dirks <j.dirks@outlook.com>
2024-05-16 00:32:54 +0200

    add some keymaps for todo comments
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- init.lua
+++ init.lua
@@ -615,6 +615,17 @@ require("lazy").setup({
 		event = "VimEnter",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
+		init = function()
+			vim.keymap.set("n", "]t", function()
+				require("todo-comments").jump_next()
+			end, { desc = "Next todo comment" })
+
+			vim.keymap.set("n", "[t", function()
+				require("todo-comments").jump_prev()
+			end, { desc = "Previous todo comment" })
+
+			vim.keymap.set("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "Telescope the todo list" })
+		end,
 	},
 
 	{ -- Collection of various small independent plugins/modules
