056044e16d386ae2855c8df60f393a6f437d609a
Jelte Dirks <j.dirks@outlook.com>
2024-05-28 13:24:37 +0200

    use lazy keys to load themes when colorpicker is used
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -3,5 +3,8 @@ return {
 		"ellisonleao/gruvbox.nvim",
 		lazy = true,
 		config = true,
+		keys = {
+			{ "<leader>sc", "<cmd>Telescope colorscheme<CR>" },
+		},
 	},
 }
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,5 +3,8 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = true,
 		config = true,
+		keys = {
+			{ "<leader>sc", "<cmd>Telescope colorscheme<CR>" },
+		},
 	},
 }
