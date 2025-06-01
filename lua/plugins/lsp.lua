12777f0dd2c67b6f510f933d19d311487840ff12
Jelte Dirks <j.dirks@outlook.com>
2025-06-01 23:04:12 +0200

    add dap ui library to lazydev as recommended
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,7 @@ return {
         ft = "lua",
         opts = {
           library = {
+            "nvim-dap-ui",
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
