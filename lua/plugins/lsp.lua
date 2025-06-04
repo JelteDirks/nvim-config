b5e89f01700c4df83140e0e46db262160a96c4fa
Jelte Dirks <j.dirks@outlook.com>
2025-06-04 10:08:46 +0200

    create less distracting diagnostic virtual text
    It removes diagnostics while in insert mode, add the diagnostics only to
    the current line. Can still use ]d [d to move between diagnostic
    messages or <leader>fd to find all diagnostics using telesocpe.
    
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,7 +58,26 @@ return {
         "texlab",
       })
 
-      vim.diagnostic.config({ virtual_text = true })
+      vim.diagnostic.config({
+        virtual_text = {
+          current_line = true,
+        },
+      })
+
+      local hide_diag_group = vim.api.nvim_create_augroup("hide-diagnostics", { clear = true })
+      vim.api.nvim_create_autocmd("InsertEnter", {
+        group = hide_diag_group,
+        callback = function()
+          vim.diagnostic.enable(false)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        group = hide_diag_group,
+        callback = function()
+          vim.diagnostic.enable(true)
+        end,
+      })
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true }),
